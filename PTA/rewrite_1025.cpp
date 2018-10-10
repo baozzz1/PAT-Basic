655929897c23db3f12cf6238c16b37278e600d29
鲍建军 <baojianjun786@outlook.com>
2018-10-11 00:36:33 +0800

    Solve problem 1025.

--- PTA/rewrite_1025.cpp
+++ PTA/rewrite_1025.cpp
@@ -21,27 +21,19 @@ void swapData(Point &p1, Point &p2) {
 	p1.Data = p2.Data;
 	p2.Data = temp;
 }
-void swapPoint(Point &p1, Point &p2) {
-	auto temp = p1;
-	p1 = p2;
-	p2 = temp;
-}
 void print(const Point &p) {
 	cout << p.Data.first << " " << p.Data.second << " ";
-	if (p.isEnd)
-		cout << "-1";
-	else
-		cout << p.Next;
+	cout << p.Next;
 	cout << endl;
 }
 
-int main() {
+int P1025_rewrite() {
 	string start;
 	int N, K;
 	cin >> start >> N >> K;
 	string addr, next;
 	int data;
-	vector<Point> points;
+	list<Point> points;
 	int n = N;
 	while (n--) {
 		cin >> addr >> data >> next;
@@ -49,13 +41,28 @@ int main() {
 	}
 
 	string lastNext = start;
-	list<Point> lst;
+	vector<Point> res;
 	while (lastNext != "-1") {
-		lst.push_back(*find_if(points.begin(), points.end(), [lastNext](const Point &p) {return p.Data.first == lastNext; }));
-		lastNext=
+		auto iter = find_if(points.begin(), points.end(), [lastNext](const Point &p) {return p.Data.first == lastNext; });
+		res.push_back(*iter);
+		lastNext = iter->Next;
+		points.erase(iter);
 	}
+	N = res.size();
+	int times = N / K;
+	for (int i = 0; i < times; i++) {
+		for (int j = 0; j < K / 2; j++) {
+			swapData(res[i*K + j], res[(i + 1)*K - j - 1]);
+		}
+	}
+	int i = 0;
+	for (; i < N - 1; i++) {
+		res[i].Next = res[i + 1].Data.first;
+		print(res[i]);
+	}
+	res[i].Next = "-1";
+	print(res[i]);
 
-
-	
+	return 0;
 
 }
