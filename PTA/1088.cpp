b6cc8b17ebfaa404fa8e7b6780ce7344e78fa5e5
鲍建军 <baojianjun786@outlook.com>
2018-10-22 16:57:42 +0800

    To Problem 1088.

--- PTA/1088.cpp
+++ PTA/1088.cpp
@@ -2,11 +2,10 @@
 #include<string>
 #include<vector>
 #include<math.h>
+
 using namespace std;
 
-
-bool findJia(const int &X, const int &Y,int &jia,int &yi) {
-	int jia, yi;
+bool findJia(const int &X, const int &Y, int &jia, int &yi) {
 	for (int i = 9; i >= 1; i--) {
 		for (int j = 9; j >= 0; j--) {
 			jia = i * 10 + j;
@@ -17,13 +16,31 @@ bool findJia(const int &X, const int &Y,int &jia,int &yi) {
 	}
 	return false;
 }
-int main() {
+template <typename T>
+void ThreePeopleGo(const int &me, const T &other) {
+	if (me > other)
+		cout << "Gai";
+	else if (me < other)
+		cout << "Cong";
+	else if (me == other)
+		cout << "Ping";
+}
+
+// 丙不一定是整数！！！
+int P1088() {
 	int M, X, Y;
 	cin >> M >> X >> Y;
-	int jia, yi, bing;
+	int jia, yi;
 	if (findJia(X, Y, jia, yi)) {
-
+		double bing = (double)yi / (double)Y;
+		cout << jia << " ";
+		ThreePeopleGo(M, jia);
+		cout << " ";
+		ThreePeopleGo(M, yi);
+		cout << " ";
+		ThreePeopleGo(M, bing);
 	}
-
+	else
+		cout << "No Solution";
 	return 0;
 }
