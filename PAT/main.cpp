b929f64d75c0a5967eb76177f0585c6386eb89eb
baozzz1 <baojianjun786@outlook.com>
2018-12-07 00:43:40 +0800

    1

--- PAT/main.cpp
+++ PAT/main.cpp
@@ -1,9 +1,15 @@
 #include<iostream>
 #include<string>
+#include<vector>
+#include<algorithm>
+#include<stdio.h>
+#include<math.h>
+#include<map>
+#include<set>
+#include<iomanip>
 using namespace std;
+
 int main() {
-
-
 
 	return 0;
 }
